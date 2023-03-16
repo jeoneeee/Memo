0568a9e7c844f9eb3d1a903068116a95c5ab7451
jeoneeee <wldus677@gmail.com>
2023-03-16 22:09:37 +0900

    [Feat] 메모 모델 생성

---
+++ Memo/Memo/Model/Model.swift
@@ -0,0 +1,23 @@
+//
+//  Model.swift
+//  Memo
+//
+//  Created by 이지연 on 2023/03/16.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "안녕하세요"),
+        Memo(content: "반갑습니다")
+    ]
+}
