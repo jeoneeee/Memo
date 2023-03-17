c3e314b70e1c1042693f7386d1a39564ce17b6c7
jeoneeee <wldus677@gmail.com>
2023-03-17 12:21:22 +0900

    [Feat] Memo 앱 기능 구현

---
+++ Memo/Memo/UIViewController+Alert.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+Alert.swift
+//  Memo
+//
+//  Created by 이지연 on 2023/03/16.
+//
+
+import UIKit
+
+extension UIViewController {
+    func alert(title: String = "알림", message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
